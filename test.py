dacfd880a982256536bd83519ef79b7eaa9d2540
Tohka-Man-AAA <antoshechkasiu@gmail.com>
2025-12-13 22:51:45 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-n = int(input())
-a = [int(input()) for i in range(n)]
 
-#сортировка
-for i in range(3):
-    for k in range(n - 1 - i):
-        if a[k] > a[k + 1]:
-            a[k], a[k + 1] = a[k + 1], a[k]
-
-# Вывод
-print(" ".join(map(str, a)) + " ")
