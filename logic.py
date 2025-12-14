cbcd287bfbc4b66ae2133543c461f606e4a93ad0
Tohka-Man-AAA <antoshechkasiu@gmail.com>
2025-12-14 09:24:21 +0300

    Update logic.py

--- logic.py
+++ logic.py
@@ -72,25 +72,44 @@ class DatabaseManager:
 
 
     def get_users(self):
-        conn = sqlite3.connect('data.db')
+        conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('SELECT')
+            cur.execute('SELECT *  FROM users')
         return [x[0] for x in cur.fetchall()] 
         
     def get_prize_img(self, prize_id):
-        conn = sqlite3.connect('data.db')
+        conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('SELECT')
+            cur.execute('SELECT image FROM prizes WHERE prize_id=?', (prize_id))
         return cur.fetchall()[0][0]
 
     def get_random_prize(self):
-        conn = sqlite3.connect('data.db')
+        conn = sqlite3.connect(self.database)
         with conn:
             cur = conn.cursor()
-            cur.execute('SELECT')
-        return cur.fetchall()[0]
+            cur.execute('SELECT * FROM prizes WHERE used = 0 ORDER BY RANDOM()')
+            return cur.fetchall()[0]
+
+    def get_winners_count(self, prize_id):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('SELECT COUNT(*) FROM winners WHERE prize_id=?', (prize_id,))
+            return cur.fetchall()[0][0]
+
+    def get_rating(self):
+        conn = sqlite3.connect(self.database)
+        with conn:
+            cur = conn.cursor()
+            cur.execute('''
+    SELECT users.user_name, COUNT(winners.prize_id) AS count_prize FROM winners
+    INNER JOIN users ON users.user_id = winners.user_id 
+    GROUP BY winners.user_id 
+    ORDER BY count_prize DESC LIMIT 10
+    ''')
+            return cur.fetchall()
     
   
 def hide_img(img_name):
